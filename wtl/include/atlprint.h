5826e48bce72b2ff55d2c2da2d99ee9cfbc46162
gilad_no <gilad_no@4beafcb9-bf40-0410-b8f5-b741cf90ec0e>
2006-12-02 10:53:11 +0000

    Fixed a possible memory leak when using CRT functions while printing
    git-svn-id: https://wtl.svn.sourceforge.net/svnroot/wtl/trunk@209 4beafcb9-bf40-0410-b8f5-b741cf90ec0e

--- wtl/include/atlprint.h
+++ wtl/include/atlprint.h
@@ -633,12 +633,17 @@ public:
 			return StartHelper();
 		}
 
-		// Create a thread and return
-		DWORD dwThreadID = 0;
+#if !defined(_ATL_MIN_CRT) && defined(_MT)
+		HANDLE hThread = (HANDLE)_beginthreadex(NULL, 0, UINT (WINAPI*)(void*))StartProc, this, 0, (UINT*)&dwThreadID);
+#else
 		HANDLE hThread = ::CreateThread(NULL, 0, StartProc, (void*)this, 0, &dwThreadID);
+#endif
+		if (hThread == NULL)
+			return false;
+
 		::CloseHandle(hThread);
 
-		return (hThread != NULL);
+		return true;
 	}
 
 // Implementation
