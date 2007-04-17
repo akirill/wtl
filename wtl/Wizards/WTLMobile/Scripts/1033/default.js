b1f7f38c74077a9c21199d7fc9e56338fdf7d057
nenadstefanovic <nenadstefanovic@4beafcb9-bf40-0410-b8f5-b741cf90ec0e>
2007-04-17 04:38:37 +0000

    Changed the link to CPL on opensource.org
    git-svn-id: https://wtl.svn.sourceforge.net/svnroot/wtl/trunk@292 4beafcb9-bf40-0410-b8f5-b741cf90ec0e

--- wtl/Wizards/WTLMobile/Scripts/1033/default.js
+++ wtl/Wizards/WTLMobile/Scripts/1033/default.js
@@ -1,3 +1,14 @@
+// Windows Template Library - WTL version 8.0
+// Copyright (C) Microsoft Corporation. All rights reserved.
+//
+// This file is a part of the Windows Template Library.
+// The use and distribution terms for this software are covered by the
+// Common Public License 1.0 (http://opensource.org/osi3.0/licenses/cpl1.0.php)
+// which can be found in the file CPL.TXT at the root of this distribution.
+// By using this software in any fashion, you are agreeing to be bound by
+// the terms of this license. You must not remove this notice, or
+// any other, from this software.
+
 
 var ProjWiz;
 ProjWiz = new ActiveXObject("ProjWiz.SDProjWiz2.2");
