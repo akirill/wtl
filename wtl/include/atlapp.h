12448120a40788521ddd5021678f55474a023e61
nenadstefanovic <nenadstefanovic@4beafcb9-bf40-0410-b8f5-b741cf90ec0e>
2007-04-22 08:11:14 +0000

    Fix for change 1.24 - using global wvsprintf
    git-svn-id: https://wtl.svn.sourceforge.net/svnroot/wtl/trunk@298 4beafcb9-bf40-0410-b8f5-b741cf90ec0e

--- wtl/include/atlapp.h
+++ wtl/include/atlapp.h
@@ -858,7 +858,7 @@ namespace SecureHelper
 		return _vstprintf_s(lpstrBuff, cchBuff, lpstrFormat, args);
 #else
 		cchBuff;   // Avoid unused argument warning
-		return ATL::wvsprintf(lpstrBuff, lpstrFormat, args);
+		return ::wvsprintf(lpstrBuff, lpstrFormat, args);
 #endif
 	}
 
